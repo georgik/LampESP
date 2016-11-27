df83fd2e0de4607ec2db7dc95f3851c46e4bfd32
Juraj Michalek <juraj.michalek@ysoft.com>
2016-11-27 11:40:27 +0100

    persist information about relay state

--- src/LampServer.ino
+++ src/LampServer.ino
@@ -155,9 +155,9 @@ void handleNotFound(){
 void setRelay(int state) {
   digitalWrite(relayPin, state);
   if (state == LOW) {
-      mqttClient.publish(topicName("relay"), "off");
+      mqttClient.publish(topicName("relay"), "off", true);
   } else {
-    mqttClient.publish(topicName("relay"), "on");
+    mqttClient.publish(topicName("relay"), "on", true);
   }
 }
 
