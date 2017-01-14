a104c563fe1f511ac975edef20064166c5a17045
Juraj Michalek <juraj.michalek@gmail.com>
2017-01-14 20:25:55 +0100

    use DHT.begin when starting measurement after fail

--- src/LampDHT.ino
+++ src/LampDHT.ino
@@ -11,15 +11,27 @@ int dhtFailCounter = 0;
 float temperatureCorrection = 0;
 
 void logDHTMeasurement() {
+  // Initialize DHT in case of previous failure
+  if (dhtFailCounter > 1) {
+    dht->begin();
+  }
+
   float h = dht->readHumidity();
   // Read temperature as Celsius (the default)
   float t = dht->readTemperature(isFahrenheit) + temperatureCorrection;
+
+  // Second attempt to read values using initialization in case of failure
+  if (isnan(h) || isnan(t)) {
+    dht->begin();
+    h = dht->readHumidity();
+    t = dht->readTemperature(isFahrenheit) + temperatureCorrection;
+  }
 
   // Check if any reads failed and exit early (to try again).
   if (isnan(h) || isnan(t)) {
     Serial.println("Failed to read from DHT sensor!");
     dhtFailCounter++;
-    if (dhtFailCounter == 3) {
+    if (dhtFailCounter == 5) {
       isDHTEnabled = false;
       dhtTask->disable();
       dhtFailCounter = 0;
@@ -27,6 +39,8 @@ void logDHTMeasurement() {
       sendMessage("humidity", "n/a");
     }
     return;
+  } else {
+    dhtFailCounter = 0;
   }
 
   // Send value only when it changed
@@ -55,6 +69,7 @@ void setupDHT(bool isEnabled, int pin) {
   temperatureCorrection = getTemperatureCorrection();
 
   dht = new DHT(pin, DHT22);
+  dhtFailCounter = 0;
   dht->begin();
 
   // Meassure once per minute. It seems that 5 second measurement is heating up
